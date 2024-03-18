8cf220151b113e3fe11f03f0a3124fc557564122
Satyamraj1234 <satyam.bvm1122@gmail.com>
2024-03-18 22:26:07 +0530

    DSA

--- ArrayExample.java
+++ ArrayExample.java
@@ -12,11 +12,47 @@ public class ArrayExample {
         }
         return array;
     }
+    public int noOfOccurence(int[] array,int n){
+        
+        int count = 0;
+        for(int i=0; i<array.length; i++){
+            if (array[i]==n) {
+                count++;
+                
+            }
+
+        }
+        
+        return count;
+
+    }
+    public int lastOccurenceIndex(int[]array ,int n){
+        int lastindex=-1;
+        for(int i=0; i<array.length; i++){
+            if (array[i]==n ) {
+                lastindex = i;
+            }
+        }
+        
+        return lastindex;
+
+    }
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         ArrayExample a= new ArrayExample();
-        a.creatArray();
-        
+        int[] array=a.creatArray();
+        System.out.println("Enter the number to check the occurrences:");
+        int n = sc.nextInt();
+        int occurrences= a.noOfOccurence( array,n);
+        System.out.println("The number " + n + " occurs " + occurrences + " times in the array.");
+       
+        System.out.println("last occurence is at index"+ a.lastOccurenceIndex(array, occurrences));
+
+
+
     }
     
 }
+
+
