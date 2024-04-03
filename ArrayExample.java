ec2d14deed612e99fd386447514f0c9fd5486a2e
Satyamraj1234 <satyam.bvm1122@gmail.com>
2024-04-03 16:21:43 +0530

    rotate array and present array question solved

--- ArrayExample.java
+++ ArrayExample.java
@@ -129,6 +129,29 @@ public class ArrayExample {
             
         }
         return array;
+    }
+    public int[] presentArray(int [] array){
+        int [] frequency = new int[100005];
+        for(int i=0; i<array.length; i++){
+            frequency[array[i]]=frequency[array[i]]+1;
+
+        }
+        return frequency;
+
+    }
+    public int[] rotateArray(int[]array ,int k){
+        int n = array.length;
+        int [] ans = new int[n];
+        k = k%n;
+        int j=0;
+        for(int i=n-k; i<n; i++){
+            ans[j++]=array[i];
+        }
+        for(int i= 0; i<n-k; i++){
+            ans[j++]=array[i];
+        }
+        return ans;
+
 
     }
 
@@ -136,28 +159,49 @@ public class ArrayExample {
         Scanner sc = new Scanner(System.in);
         ArrayExample a= new ArrayExample();
         int[] array=a.creatArray();
-        System.out.println("Enter the number to check the occurrences:");
-        int n = sc.nextInt();
-        int occurrences= a.noOfOccurence( array,n);
-        System.out.println("The number " + n + " occurs " + occurrences + " times in the array.");
+    //     System.out.println("Enter the number to check the occurrences:");
+    //     int n = sc.nextInt();
+    //     int occurrences= a.noOfOccurence( array,n);
+    //     System.out.println("The number " + n + " occurs " + occurrences + " times in the array.");
        
-        System.out.println("last occurence is at index"+ a.lastOccurenceIndex(array, occurrences));
-        System.out.println("enter target sum");
-        int target = sc.nextInt();
-        int targets= a.targetsum(array,target);
-        System.out.println("total no of pairs are "+targets);
-       int unique= a.findUnique(array);
-        System.out.println("unique no is "+unique);
-     int firstrepeatingno =a.firtRepeatingNumber(array);
-     System.out.println("first repeating number is"+firstrepeatingno);
-    int secondmax= a.secondMax(array);
-     System.out.println("seconf maax value of array is "+secondmax);
-     int[] ans =a.reverseArray(array);
-     System.out.println("reversed array are");
-    a.printArray(ans);
-     int[] r= a.ReverseArray(array);
+    //     System.out.println("last occurence is at index"+ a.lastOccurenceIndex(array, occurrences));
+    //     System.out.println("enter target sum");
+    //     int target = sc.nextInt();
+    //     int targets= a.targetsum(array,target);
+    //     System.out.println("total no of pairs are "+targets);
+    //    int unique= a.findUnique(array);
+    //     System.out.println("unique no is "+unique);
+    //  int firstrepeatingno =a.firtRepeatingNumber(array);
+    //  System.out.println("first repeating number is"+firstrepeatingno);
+    // int secondmax= a.secondMax(array);
+    //  System.out.println("seconf maax value of array is "+secondmax);
+    //  int[] ans =a.reverseArray(array);
+    //  System.out.println("reversed array are");
+    // a.printArray(ans);
+    //  int[] r= a.ReverseArray(array);
 
-     a.printArray(r);
+    //  a.printArray(r);
+    //  int[] presentArray= a.presentArray(array);
+    //  System.out.println("enter the number of query");
+    //  int query= sc.nextInt();
+    //  while (query>0) {
+    //     System.out.println("enter number to be searched");
+    //     int x = sc.nextInt();
+    //     if(presentArray[x]>0){
+    //         System.out.println("yes present ");
+
+    //     }else{
+    //         System.out.println("not present");
+    //     }
+    //     query--;
+        
+    //  }
+    System.out.println("enter k");
+    int  k= sc.nextInt();
+    int[] rotate=a.rotateArray(array, k);
+    a.printArray(rotate);
+
+
 
 
 
